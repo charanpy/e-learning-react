2deb61d262fc60a2c1d258fca3bc565fd3916d34
charanmurugan <murugancharan245@gmail.com>
2022-04-29 09:10:49 +0530

    cleanup

--- src/components/explore-courses/ExploreCourses.component.jsx
+++ src/components/explore-courses/ExploreCourses.component.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { useQuery } from 'react-query';
-import request from '../../lib/fetch';
-import Course from './Course.component';
-import './course.css';
+import React from "react";
+import { useQuery } from "react-query";
+import request from "../../lib/fetch";
+import Course from "./Course.component";
+import "./course.css";
 
 const ExploreCourses = () => {
-  const { data } = useQuery('explore-course', () => request('/course'));
+  const { data } = useQuery("explore-course", () => request("/course"));
   return (
     <>
       <Course courses={data} />
--- src/components/video-details/VideoBanner.component.jsx
+++ src/components/video-details/VideoBanner.component.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import VideoContentComponent from "./VideoContent.component";
 import VideoDetailContainer from "./VideoDetailContainer.component";
 import "./video-details.css";
+import { useQuery } from "react-query";
+import request from "../../lib/fetch";
 
 const VideoBannerComponent = () => {
+  const { data } = useQuery("explore-video", () => request("/video"));
+
   return (
     <div className="flex-row video-banner-component">
       <VideoDetailContainer />
