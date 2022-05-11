c4d47c2fac798b8df16874d04f47903ec5159d5c
charanmurugan <murugancharan245@gmail.com>
2022-05-11 12:09:49 +0530

    added 404 page

--- src/pages/not-found/NotFound.page.jsx
+++ src/pages/not-found/NotFound.page.jsx
@@ -1,10 +1,41 @@
-import React from 'react';
-import notFoundImg from '../../assets/notFound.jpg';
+import React from "react";
+import { Link } from "react-router-dom";
+import not from "../../assets/404.png";
+import "./notfound.css";
 
 const NotFound = () => {
   return (
-    <div className='flex-row centerAll container'>
-      <img src={notFoundImg} alt='not found' />
+    <div className="flex-row centerAll">
+      <div className="container">
+        <h1 class="first-four">4</h1>
+        <div class="cog-wheel1">
+          <div class="cog1">
+            <div class="top"></div>
+            <div class="down"></div>
+            <div class="left-top"></div>
+            <div class="left-down"></div>
+            <div class="right-top"></div>
+            <div class="right-down"></div>
+            <div class="left"></div>
+            <div class="right"></div>
+          </div>
+        </div>
+
+        <div class="cog-wheel2">
+          <div class="cog2">
+            <div class="top"></div>
+            <div class="down"></div>
+            <div class="left-top"></div>
+            <div class="left-down"></div>
+            <div class="right-top"></div>
+            <div class="right-down"></div>
+            <div class="left"></div>
+            <div class="right"></div>
+          </div>
+        </div>
+        <h1 class="second-four">4</h1>
+      </div>
+      <p class="wrong-para">Uh Oh! Page not found!</p>
     </div>
   );
 };
